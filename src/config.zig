3dac3bb8fe2f07e5d746624795bc70a497f4c956
kewuaa <1692525710@qq.com>
2025-12-29 20:02:51 +0800

    update config

--- src/config.zig
+++ src/config.zig
@@ -4,11 +4,12 @@ const wayland = @import("wayland");
 const river = wayland.client.river;
 
 const binding = @import("binding.zig");
+const Context = @import("context.zig");
 
-const alt: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.mod1);
-const super: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.mod4);
-const ctrl: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.ctrl);
-const shift: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.shift);
+const Alt: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.mod1);
+const Super: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.mod4);
+const Ctrl: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.ctrl);
+const Shift: u32 = @intFromEnum(river.SeatV1.Modifiers.Enum.shift);
 const Button = struct {
     const left = 0x110;
     const right = 0x111;
@@ -34,123 +35,220 @@ const BorderColor = struct {
     urgent: u32,
 };
 
+
 pub const Mode = enum {
     default,
     passthrough,
 };
 
-pub const border_width = 3;
+pub var border_width: i32 = 3;
 pub const border_color: BorderColor = .{
-    .focus = 0xA54242,
-    .unfocus = 0x707880,
+    .focus = 0xffc777,
+    .unfocus = 0x828bb8,
     .urgent = 0xff0000,
 };
+
+
+pub var layout: struct {
+    tile: @import("layout/tile.zig"),
+    monocle: @import("layout/monocle.zig"),
+    scroller: @import("layout/scroller.zig"),
+} = .{
+    .tile = .{
+        .nmaster = 1,
+        .mfact = 0.55,
+        .gap = 10,
+    },
+    .monocle = .{
+        .gap = 10,
+    },
+    .scroller = .{
+        .mfact = 0.6,
+        .gap = 10,
+    }
+};
+
+
+fn modify_mfact(context: *const Context, arg: *const binding.Arg) void {
+    if (context.current_output) |output| {
+        switch (output.current_layout()) {
+            .tile => layout.tile.mfact += arg.f,
+            .scroller => layout.scroller.mfact += arg.f,
+            else => {}
+        }
+    }
+}
 
 
 pub const xkb_bindings = blk: {
     const bindings = [_]XkbBinding {
         .{
             .keysym = Keysym.Escape,
-            .modifiers = super,
-            .action = .{ .switch_mode = .passthrough }
+            .modifiers = Super|Shift,
+            .action = .{ .switch_mode = .{ .mode = .passthrough } }
         },
         .{
             .mode = .passthrough,
             .keysym = Keysym.Escape,
-            .modifiers = super,
-            .action = .{ .switch_mode = .default }
+            .modifiers = Super,
+            .action = .{ .switch_mode = .{ .mode = .default } }
         },
 
         .{
             .keysym = Keysym.q,
-            .modifiers = super,
+            .modifiers = Super|Shift,
             .action = .quit,
         },
         .{
             .keysym = Keysym.c,
-            .modifiers = super,
+            .modifiers = Super|Shift,
             .action = .close,
         },
         .{
-            .keysym = Keysym.p,
-            .modifiers = super,
-            .action = .{ .spawn = &[_][]const u8 { "wmenu-run" } },
+            .keysym = Keysym.Return,
+            .modifiers = Super,
+            .action = .zoom,
+        },
+        .{
+            .keysym = Keysym.l,
+            .modifiers = Super,
+            .action = .{ .custom_fn = .{ .func = &modify_mfact, .arg = .{ .f = 0.01 } } },
+        },
+        .{
+            .keysym = Keysym.h,
+            .modifiers = Super,
+            .action = .{ .custom_fn = .{ .func = &modify_mfact, .arg = .{ .f = -0.01 } } },
+        },
+        .{
+            .keysym = Keysym.j,
+            .modifiers = Super,
+            .action = .{ .focus_iter = .{ .direction = .forward, .skip_floating = true, } },
+        },
+        .{
+            .keysym = Keysym.k,
+            .modifiers = Super,
+            .action = .{ .focus_iter = .{ .direction = .reverse, .skip_floating = true } },
+        },
+        .{
+            .keysym = Keysym.j,
+            .modifiers = Super|Alt,
+            .action = .{ .focus_iter = .{ .direction = .forward } },
+        },
+        .{
+            .keysym = Keysym.k,
+            .modifiers = Super|Alt,
+            .action = .{ .focus_iter = .{ .direction = .reverse } },
         },
         .{
             .keysym = Keysym.f,
-            .modifiers = super,
+            .modifiers = Super,
             .action = .{ .toggle_fullscreen = .{ .in_window = true } },
         },
         .{
             .keysym = Keysym.f,
-            .modifiers = super|shift,
+            .modifiers = Super|Shift,
             .action = .{ .toggle_fullscreen = .{} },
         },
         .{
-            .keysym = Keysym.l,
-            .modifiers = super|ctrl,
-            .action = .{ .move = .{ .horizontal = 10 } }
+            .keysym = Keysym.f,
+            .modifiers = Super|Ctrl,
+            .action = .toggle_floating,
         },
         .{
-            .keysym = Keysym.h,
-            .modifiers = super|ctrl,
-            .action = .{ .move = .{ .horizontal = -10 } }
+            .keysym = Keysym.f,
+            .modifiers = Super|Ctrl|Alt,
+            .action = .{ .switch_layout = .{ .layout = .float } },
         },
         .{
-            .keysym = Keysym.j,
-            .modifiers = super|ctrl,
-            .action = .{ .move = .{ .vertical = 10 } }
+            .keysym = Keysym.t,
+            .modifiers = Super,
+            .action = .{ .switch_layout = .{ .layout = .tile } },
         },
         .{
-            .keysym = Keysym.k,
-            .modifiers = super|ctrl,
-            .action = .{ .move = .{ .vertical = -10 } }
+            .keysym = Keysym.m,
+            .modifiers = Super,
+            .action = .{ .switch_layout = .{ .layout = .monocle } },
         },
         .{
-            .keysym = Keysym.l,
-            .modifiers = super|alt,
-            .action = .{ .resize = .{ .horizontal = 10 } }
-        },
-        .{
-            .keysym = Keysym.h,
-            .modifiers = super|alt,
-            .action = .{ .resize = .{ .horizontal = -10 } }
-        },
-        .{
-            .keysym = Keysym.j,
-            .modifiers = super|alt,
-            .action = .{ .resize = .{ .vertical = 10 } }
-        },
-        .{
-            .keysym = Keysym.k,
-            .modifiers = super|alt,
-            .action = .{ .resize = .{ .vertical = -10 } }
+            .keysym = Keysym.s,
+            .modifiers = Super,
+            .action = .{ .switch_layout = .{ .layout = .scroller } },
         },
         .{
             .keysym = Keysym.l,
-            .modifiers = super|ctrl|shift,
-            .action = .{ .snap = .{ .right = true } }
+            .modifiers = Super|Ctrl,
+            .action = .{ .move = .{ .step = .{ .horizontal = 10 } } }
         },
         .{
             .keysym = Keysym.h,
-            .modifiers = super|ctrl|shift,
-            .action = .{ .snap = .{ .left = true } }
+            .modifiers = Super|Ctrl,
+            .action = .{ .move = .{ .step = .{ .horizontal = -10 } } }
         },
         .{
             .keysym = Keysym.j,
-            .modifiers = super|ctrl|shift,
-            .action = .{ .snap = .{ .bottom = true } }
+            .modifiers = Super|Ctrl,
+            .action = .{ .move = .{ .step = .{ .vertical = 10 } } }
         },
         .{
             .keysym = Keysym.k,
-            .modifiers = super|ctrl|shift,
-            .action = .{ .snap = .{ .top = true } }
+            .modifiers = Super|Ctrl,
+            .action = .{ .move = .{ .step = .{ .vertical = -10 } } }
+        },
+        .{
+            .keysym = Keysym.l,
+            .modifiers = Super|Alt,
+            .action = .{ .resize = .{ .step = .{ .horizontal = 10 } } }
+        },
+        .{
+            .keysym = Keysym.h,
+            .modifiers = Super|Alt,
+            .action = .{ .resize = .{ .step = .{ .horizontal = -10 } } }
+        },
+        .{
+            .keysym = Keysym.j,
+            .modifiers = Super|Alt,
+            .action = .{ .resize = .{ .step = .{ .vertical = 10 } } }
+        },
+        .{
+            .keysym = Keysym.k,
+            .modifiers = Super|Alt,
+            .action = .{ .resize = .{ .step = .{ .vertical = -10 } } }
+        },
+        .{
+            .keysym = Keysym.l,
+            .modifiers = Super|Ctrl|Shift,
+            .action = .{ .snap = .{ .edges = .{ .right = true } } }
+        },
+        .{
+            .keysym = Keysym.h,
+            .modifiers = Super|Ctrl|Shift,
+            .action = .{ .snap = .{ .edges = .{ .left = true } } }
+        },
+        .{
+            .keysym = Keysym.j,
+            .modifiers = Super|Ctrl|Shift,
+            .action = .{ .snap = .{ .edges = .{ .bottom = true } } }
+        },
+        .{
+            .keysym = Keysym.k,
+            .modifiers = Super|Ctrl|Shift,
+            .action = .{ .snap = .{ .edges = .{ .top = true } } }
         },
         .{
             .keysym = Keysym.@"0",
-            .modifiers = super,
-            .action = .{ .set_output_tag = 0xffffffff }
-        }
+            .modifiers = Super,
+            .action = .{ .set_output_tag = .{ .tag = 0xffffffff } }
+        },
+        .{
+            .keysym = Keysym.p,
+            .modifiers = Super,
+            .action = .{ .spawn = .{ .argv = &[_][]const u8 { "wmenu-run" } } },
+        },
+        .{
+            .keysym = Keysym.Return,
+            .modifiers = Super|Shift,
+            .action = .{ .spawn = .{ .argv = &[_][]const u8 { "foot" } } },
+        },
     };
 
     const tag_num = 9;
@@ -158,23 +256,23 @@ pub const xkb_bindings = blk: {
     for (0..tag_num) |i| {
         tag_binddings[i*4] = .{
             .keysym = Keysym.@"1"+i,
-            .modifiers = super,
-            .action = .{ .set_output_tag = 1 << i },
+            .modifiers = Super,
+            .action = .{ .set_output_tag = .{ .tag = 1 << i } },
         };
         tag_binddings[i*4+1] = .{
             .keysym = Keysym.@"1"+i,
-            .modifiers = super|shift,
-            .action = .{ .set_window_tag = 1 << i },
+            .modifiers = Super|Shift,
+            .action = .{ .set_window_tag = .{ .tag = 1 << i } },
         };
         tag_binddings[i*4+2] = .{
             .keysym = Keysym.@"1"+i,
-            .modifiers = super|ctrl,
-            .action = .{ .toggle_output_tag = 1 << i },
+            .modifiers = Super|Ctrl,
+            .action = .{ .toggle_output_tag = .{ .mask = 1 << i } },
         };
         tag_binddings[i*4+3] = .{
             .keysym = Keysym.@"1"+i,
-            .modifiers = super|ctrl|shift,
-            .action = .{ .toggle_window_tag = 1 << i },
+            .modifiers = Super|Ctrl|Shift,
+            .action = .{ .toggle_window_tag = .{ .mask = 1 << i } },
         };
     }
 
@@ -184,12 +282,12 @@ pub const xkb_bindings = blk: {
 pub const pointer_bindings = [_]PointerBinding {
     .{
         .button = Button.left,
-        .modifiers = super,
+        .modifiers = Super,
         .action = .pointer_move,
     },
     .{
         .button = Button.right,
-        .modifiers = super,
+        .modifiers = Super,
         .action = .pointer_resize,
     },
 };
